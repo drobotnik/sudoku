e759347070635579b800724ebfc37bd431cc087e
drobotnik <yahia@ELSLABM-155865.science.regn.net>
2016-05-20 18:21:57 +0100

    prototype binary pruner

--- blah.py
+++ blah.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+
+group = [
+    [0, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0]
+]
+
+
+df = pd.DataFrame(group)
+df.columns = ['a', 'b']
+
+print(df)
+print()
+
+for col in df.columns:
+    print(df.groupby(col).count().max())
