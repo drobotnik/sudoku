5f8ff9f317941f8a91c6153ea75e0e7110a7ca9d
drobotnik <yahia@ELSLABM-155865.science.regn.net>
2016-05-20 17:34:44 +0100

    removed old knuth implementation

--- mastermind.py
+++ mastermind.py
@@ -42,7 +42,6 @@ class Mastermind(object):
         if strategy == 1:
             self.remaining_opts = list(product(range(num_colours), repeat=num_pegs))
             self.possible_opts = list(product(range(num_colours), repeat=num_pegs))
-            self.knuth_key = lambda test_guess: max(Counter(check(code, test_guess) for code in self.remaining_opts).values())
 
     def make_guess(self, guess):
         response = self.secret.give_guess_response(guess)
@@ -57,6 +56,23 @@ class Mastermind(object):
                 return False
         return True
 
+    def knuth_key(self, test_guess):
+        """
+        Key explanation:
+        For your test code
+        check that to all the remaining possible secrets
+        see how many of the same type of responses you would get for that code against all possible secrets.
+        The higher the number, the worse it is as that will be more ambiguous.
+        This is because it could be many different secrets which would give you that response from check() given that code.
+        Find the highest value from these occurances (worst case).
+        That is the score for that code.
+        ie. If you got that response, how many different secrets would have given you that code
+        """
+        test_checker = (check(code, test_guess) for code in self.remaining_opts)  # For each possible code, see what response you would get with the guess you are testing
+        group_responses = Counter(test_checker)  # Add up all the different possibilities. eg (0, 2): 50 means-> If you get a response of (0, 2), then it can only be one of 50 possibilites
+        wc = group_responses.values()  # We dont care about the different values since we don't know what it will, just how many times each option might appear
+        return max(wc)  # We are only interested in the worst case
+
     def get_next_guess(self):
         if self.strategy == 0:
             next_guess = next(self.possible_opts)
@@ -65,21 +81,21 @@ class Mastermind(object):
 
         elif self.strategy == 1:
             last_guess, last_response = self.guesses[-1]
-            filtered_codes = []  # c for c in codes if check(guess, c) == feedback]
-            for c in self.remaining_opts:  # for each remaining possible code
-                if check(c, last_guess) == last_response:  # Does that code match what we learnt from the last guess?
-                    filtered_codes += [c]  # If so, add it to the list of remaining possible codes
+            filtered_codes = []
+            for remaining_opt in self.remaining_opts:  # for each remaining possible code
+                if check(remaining_opt, last_guess) == last_response:  # Does that code match what we learnt from the last guess?
+                    filtered_codes += [remaining_opt]  # If so, add it to the new list of remaining possible codes
             self.remaining_opts = filtered_codes  # Set codes to only be the remaining possibilities
 
             if len(self.remaining_opts) == 1:
                 next_guess = self.remaining_opts[0]
             else:
                 next_guess = min(self.possible_opts, key=self.knuth_key)  # Find the code which would give you the most unambiguous response in the worst case
+
         else:
             next_guess = next(self.possible_opts)
 
         return next_guess
-
 
     def run(self):
         chosen_guess = [int(x >= self.num_pegs / 2) for x in range(self.num_pegs)]
@@ -119,7 +135,7 @@ def single_test(code, num_colours=6, num_pegs=4, strategy=0, duplicates=True):
     print("Num Guesses: {}".format(len(m.guesses)))
 
 
-single_test([5, 1, 0, 3], num_colours=6, num_pegs=4, strategy=2)
+single_test([0, 0, 0, 6], num_colours=7, num_pegs=4, strategy=1)
 
 
 def test_all_combinations(colours=6, pegs=4, duplicates=True):
@@ -143,54 +159,3 @@ def test_all_combinations(colours=6, pegs=4, duplicates=True):
 
 
 # test_all_combinations(pegs=4, duplicates=True)
-
-
-ALL_CODES = list(product(range(6), repeat=4))
-
-
-def knuth(secret):
-    """Run Knuth's 5-guess algorithm on the given secret."""
-    assert(secret in ALL_CODES)
-    codes = ALL_CODES  # Instantiate codes to be all possible codes
-    key = lambda test_guess: max(Counter(check(code, test_guess) for code in codes).values())
-    """
-    Key explanation:
-    For your test code
-    check that to all the remaining possible secrets
-    see how many of the same type of responses you would get for that code against all possible secrets.
-    The higher the number, the worse it is as that will be more ambiguous.
-    This is because it could be many different secrets which would give you that response from check() given that code.
-    Find the highest value from these occurances (worst case).
-    That is the score for that code.
-    ie. If you got that response, how many different secrets would have given you that code
-    """
-    guess = (0, 0, 1, 1)
-    guess_count = 0
-    while True:
-        feedback = check(secret, guess)
-        guess_count += 1
-        print("Guess {}: feedback {}".format(guess, feedback))
-        if guess == secret:
-            return guess_count
-
-        filtered_codes = [] # c for c in codes if check(guess, c) == feedback]
-        for c in codes:  # for each remaining possible code
-            if check(c, guess) == feedback: # Does that code match what we learnt from the last guess?
-                filtered_codes += [c]  # If so, add it to the list of remaining possible codes
-        codes = filtered_codes  # Set codes to only be the remaining possibilities
-
-        if len(codes) == 1:
-            guess = codes[0]
-        else:
-            guess = min(ALL_CODES, key=key)  # Find the code which would give you the most unambiguous response in the worst case
-
-
-# print(knuth((0, 0, 0, 0)))
-#
-# results = []
-# for secret in product(range(6), repeat=4):
-#     count = knuth(secret)
-#     print(count)
-#     results += [count]
-#
-# print(Counter(results))
